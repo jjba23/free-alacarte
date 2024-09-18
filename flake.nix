1d97a5b112ba0184cbb08d6bc37f49f5acdfdb48
Josep Bigorra <jjbigorra@gmail.com>
2024-09-18 15:58:24 +0200

    update to simpler dev

--- flake.nix
+++ flake.nix
@@ -1,55 +1,31 @@
 {
-  description = "Free a la Carte flake";
-  inputs.haskellNix.url = "github:input-output-hk/haskell.nix";
-  inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-
-  outputs = { self, nixpkgs, flake-utils, haskellNix }:
-    let
-      supportedSystems =
-        [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
-    in flake-utils.lib.eachSystem supportedSystems (system:
-      let
-        overlays = [
-          haskellNix.overlay
-          (final: prev: {
-            free-alacarte = final.haskell-nix.project' {
-              src = ./.;
-              compiler-nix-name = "ghc96";
-              shell.tools = {
-                cabal = { };
-                hlint = { };
-                haskell-language-server = { };
-              };
-              shell.buildInputs = with pkgs; [
-                gnumake
-                haskell-language-server
-                cabal-install
-                cachix
-                ormolu
-                nixfmt
-                statix
-                deadnix
-              ];
-            };
-          })
-        ];
-        pkgs = import nixpkgs {
-          inherit system overlays;
-          inherit (haskellNix) config;
-        };
-        flake = pkgs.free-alacarte.flake { };
-      in flake // {
-        legacyPackages = pkgs;
-
-        packages.default = flake.packages."free-alacarte:lib:free-alacarte";
-        # packages.test = flake.packages."free-alacarte:test:spec";
-      });
-
-  nixConfig = {
-    extra-substituters = [ "https://cache.iog.io" ];
-    extra-trusted-public-keys =
-      [ "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" ];
-    allow-import-from-derivation = "true";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
   };
+  outputs = { systems, nixpkgs, ... }:
+    let
+      eachSystem = f:
+        nixpkgs.lib.genAttrs (import systems)
+        (system: f nixpkgs.legacyPackages.${system});
+    in {
+      devShells = eachSystem (pkgs: {
+        default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            gnumake
+            haskell-language-server
+            cachix
+            ormolu
+            nixfmt
+            ghcid
+            statix
+            stack
+            deadnix
+            jq
+            awscli2
+          ];
+        };
+      });
+    };
 }
+
