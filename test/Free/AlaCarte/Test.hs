360eb86436a39fececcb2baa623cea5729dddb4a
Josep Bigorra <jjbigorra@gmail.com>
2024-09-19 15:02:29 +0200

    wire

--- test/Free/AlaCarte/Test.hs
+++ test/Free/AlaCarte/Test.hs
@@ -2,3 +2,8 @@ module Free.AlaCarte.Test where
 
 import Free.AlaCarte
 import Hspec
+
+freealacarteSpec = do
+  describe "Free a la Carte" $ do
+    it "hspec works" $ do
+      True `shouldBe` True
